d856ea25577c904e2a2273998c86c1f28c9d1e07
Kevin Taylor <hang10z@gmail.com>
2016-04-16 21:11:39 -0400

    Commit1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,44 @@
-int main ()
+#include <iostream>
+#include <string>
+
+//namespaces
+using namespace std;
+
+//declare fuctions
+void PrintIntro();
+string GetGuess();
+
+
+
+//Entry point for our application
+int main()
 {
+	PrintIntro();
+	string guess1 = GetGuess();
+	cout << "Your guess was " << guess1 << endl;
+	string guess2 = GetGuess();
+	cout << "Your guess was " << guess2 << endl;
 	return 0;
+
+}
+
+void PrintIntro()
+{
+	//introduce the game
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows!\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking of?!\n";
+	cout << endl;
+	return;
+}
+
+string GetGuess()
+{
+	// get a guess from the player
+	string Guess = "";
+	cout << "Please type in your guess: ";
+	getline(cin, Guess);
+	return Guess;
+	
 }
